64200ff26606a61c31cac223ae08fa1cc7f90174
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-03-03 16:05:51 +0300

    Изменение главной

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 
 int main() {
+    setlocale(LC_ALL, "Russian"); 
+    int x, n;
+    cout << "Введите число" << endl;
+    cin >> x;
+    cout << "Введите степень" << endl;
+    cin >> n;
+    cout << "Результат: " << power(x,n) << endl;
     return 0;
 }
