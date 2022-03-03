3cbce45d9d06f9a5a89ae6b3ae04f8ba60f542c5
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-03-03 17:55:42 +0300

    Аккуратный вывод

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 #include <iostream>
 
 int main() {
+    using std::cout;
+    using std::cin;
     setlocale(LC_ALL, "Russian"); 
     int x, n;
-    cout << "Введите число" << endl;
+    cout << "Введите число\n";
     cin >> x;
-    cout << "Введите степень" << endl;
+    cout << "Введите степень\n";
     cin >> n;
-    cout << "Результат: " << power(x,n) << endl;
+    cout << "\nРезультат: " << power(x, n);
     return 0;
 }
