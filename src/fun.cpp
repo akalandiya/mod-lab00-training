efda00896a5fd402a9a45ecbd943dca5cfc0440b
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-03-03 23:03:25 +0300

    Оформление

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,10 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n){
+int64_t power(int64_t x, uint16_t n) {
     int64_t a = 1;
-    for (int i = 0; i < n ; i++){
-    a = a * x;  
+    for (int i = 0; i < n ; i++) {
+    a = a * x;
     }
     return a;
 }
